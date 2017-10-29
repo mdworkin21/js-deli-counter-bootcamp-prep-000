4ac42bbfba19009b21452a9f7e812fb57f0323b2
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-29 19:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function currentLine(array){
   } else {
       for( var i = 0; i < array.length; i++){
       var name = array[i];
-      var position = array.indexOf() + ". "
+      var position = array.indexOf(name) + ". "
       return "The line is currently: " + position + name + ", "
 
     }
