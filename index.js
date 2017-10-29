5f7f58f339557c59537ee6c1b8082843b1a9b7fa
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-29 20:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function currentLine(array){
     if (array.length === 0){
     return "The line is currently empty."
   } else {
-      for( var i = 0; i < array.length; i++){
+      for( var i = 0; i < 5; i++){
       var name = array[i];
       var position = `${i}` + ". "
       return "The line is currently: " + position + name + ", "
