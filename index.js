9a92c3765c550efe7cf59ccfd0cd7792f0ae6fd3
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-29 20:08:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ function currentLine(array){
     if (array.length === 0){
     return "The line is currently empty."
   } else {
-      for( var i = 0; i < array.length; i++){
+      //for( var i = 0; i < array.length; i++){
       var name = array[i];
-      var position = name[i] + ". "
+      var position = (array[i] + 1) + ". "
+      while(i < array.length)
       return "The line is currently: " + position + name + ", "
 
     }
   }
-}
